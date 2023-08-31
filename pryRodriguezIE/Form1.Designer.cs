a57d8fe8c9ad9f7b0002ca7412cbd43b90808c0a
root <mgiraudo@ies21.edu.ar>
2023-08-31 13:34:47 -0300

    Agregar archivos de proyecto.

---
+++ pryRodriguezIE/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace pryRodriguezIE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cargarProveedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeProveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarProveedoresToolStripMenuItem,
+            this.registroDeProveedorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cargarProveedoresToolStripMenuItem
+            // 
+            this.cargarProveedoresToolStripMenuItem.Name = "cargarProveedoresToolStripMenuItem";
+            this.cargarProveedoresToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.cargarProveedoresToolStripMenuItem.Text = "Cargar proveedores";
+            // 
+            // registroDeProveedorToolStripMenuItem
+            // 
+            this.registroDeProveedorToolStripMenuItem.Name = "registroDeProveedorToolStripMenuItem";
+            this.registroDeProveedorToolStripMenuItem.Size = new System.Drawing.Size(135, 20);
+            this.registroDeProveedorToolStripMenuItem.Text = "Registro de proveedor";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cargarProveedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeProveedorToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
